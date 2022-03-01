603431f236dce4ec29df71142c978672ee867ff5
Infinite Flite <infinate1234flight@gmail.com>
2022-03-01 09:59:32 -0800

    Made form page. Started on math functions

--- collegeFinder.py
+++ collegeFinder.py
@@ -1,6 +1,18 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
+import math
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+def get_gpa_index(GPA):
+    if GPA == 5:
+        return 0
+    else:
+        index = 5/((GPA**2)+0.3)
+        return index
+
+def get_vonunteer_index(hours):
+    index = .2*(-hours + 100)
+    return index
 
 @app.route("/")
 def render_main():
@@ -12,7 +24,15 @@ def render_form():
 
 @app.route("/results")
 def render_results():
-    return render_template('results.html')
+    gpa = float(request.args['uGPA'])
+    pTaken = int(request.args['uAPTaken'])
+    pPassed = int(request.args['uAPpassed'])
+    vHours = int(request.args['uvHours'])
+    ASB = request.args['ASB']
+
+    collegeIndex = 100-((pTaken-pPassed)*6)-((8-pTaken)*2)-get_gpa_index(gpa)-get_vonunteer_index(vHours)
+
+    return render_template('results.html', index = collegeIndex)
 
 if __name__=="__main__":
     app.run(debug=True)
